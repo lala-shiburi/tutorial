c09c169a5251c78db3fc74906d912121379a12db
lala <nhlalala@nshiburi.co.za>
2019-07-07 02:44:26 +0200

    Changes to Hyperlinked APIS

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,11 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
@@ -32,3 +37,19 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
